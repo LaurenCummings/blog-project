a2d9e1e9b241f41f96666991e50d4bfb6a31f0ca
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-15 23:58:18 -0400

    added useEffect to store categories in state constant

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,16 @@
 import { getPostTags } from '../ApiCalls';
 import useFetch from '../components/useFetch';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Categories() {
     const { data, error, pending } = useFetch(getPostTags, {});
     const [categories, setCategories] = useState();
+
+    useEffect(() => {
+        if (data) {
+            setCategories(data);
+        }
+    }, [data]);
     
     return (
         <div>
