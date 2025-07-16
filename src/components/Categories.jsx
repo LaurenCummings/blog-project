af4ea4a4a17f7e10b3245810fa6d9a5305eb0480
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-15 23:56:16 -0400

    created categories state constant

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,10 @@
 import { getPostTags } from '../ApiCalls';
 import useFetch from '../components/useFetch';
+import { useState } from 'react';
 
 function Categories() {
     const { data, error, pending } = useFetch(getPostTags, {});
+    const [categories, setCategories] = useState();
     
     return (
         <div>
