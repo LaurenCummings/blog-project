e0811a1e50714b94b333d75ebd395b1ec2e2761f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-16 23:31:34 -0400

    moved feed content into its own component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,8 @@
 import '../css/Home.css';
-import { getPosts } from '../ApiCalls';
-import useFetch from '../components/useFetch';
-import { useState, useEffect } from 'react';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import Categories from '../components/Categories';
+import Feed from '../components/Feed';
 
 function Home() {
-    const { data, error, pending } = useFetch(getPosts, {});
-    const [posts, setPosts] = useState([]);
-    const [currentPost, setCurrentPost] = useState();
-    const [firstPost, setFirstPost] = useState(false);
-    const [lastPost, setLastPost] = useState(false);
-
-    function getNextPost() {
-        const nextPostId = currentPost.id + 1;
-        setCurrentPost(posts[nextPostId - 1]);
-    }
-
-    function getPrevPost() {
-        const prevPostId = currentPost.id - 1;
-        setCurrentPost(posts[prevPostId - 1]);
-    }
-
-    useEffect(() => {
-        if (data && data.posts && data.posts.length) {
-            setPosts(data.posts);    
-        }    
-    }, [data]);
-
-    useEffect(() => {
-        setCurrentPost(posts[0]);
-    }, [posts]);
-
-    useEffect(() => {
-        if (currentPost && currentPost.id == 1) {
-            setFirstPost(true);
-        } else {
-            setFirstPost(false);
-        }
-        if (currentPost && currentPost.id == posts.length) {
-            setLastPost(true);
-        } else {
-            setLastPost(false);
-        }
-    }, [currentPost]);
 
     return (
         <div className="home">
@@ -51,29 +10,7 @@ function Home() {
                 <Categories />
             </div>
             <div className="feed">
-                {pending ? 
-                    <h3>Loading...</h3> : null
-                }
-                {error ?
-                    <h3>{error}</h3> : null
-                }
-                {currentPost &&
-                    <div className="current-post">
-                        <p>Post #{currentPost.id}</p>
-                        <h2>{currentPost.title}</h2>
-                        <p className="post-body">{currentPost.body}</p>
-                        <div className="post-arrows">
-                            <div className={firstPost ? "arrow-hide" : "arrow-left"} onClick={getPrevPost}>
-                                <FaArrowLeft />
-                                <p>Previous</p>
-                            </div>
-                            <div className={lastPost ? "arrow-hide" : "arrow-right"} onClick={getNextPost}>
-                                <p>Next</p>
-                                <FaArrowRight />
-                            </div>
-                        </div>
-                    </div>
-                }
+                <Feed />
             </div>
             <div className="suggested-posts">
                 Suggested posts
