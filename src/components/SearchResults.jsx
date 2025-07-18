fac2e04d31b439901022a7471530eab0980fcbc9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-18 00:04:25 -0400

    added useEffect to store searchResults into state constant

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -4,9 +4,13 @@ import useFetch from '../components/useFetch';
 function SearchResults(props) {
     const { data, error, pending } = useFetch(`https://dummyjson.com/posts/search?q=${props.category}`, {});
     const [postResults, setPostResults] = useState([]);
-    console.log(data);
+    console.log(postResults);
 
-
+    useEffect(() => {
+        if (data) {
+            setPostResults(data.posts);   
+        }
+    },[data]);
 
     return (
         <div>
