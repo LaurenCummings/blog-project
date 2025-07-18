3950c3f8779f9f6246e19f35777675c1cf061c50
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-17 23:59:54 -0400

    imported useEffect into searchResults

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useFetch from '../components/useFetch';
 
 function SearchResults(props) {
     const { data, error, pending } = useFetch(`https://dummyjson.com/posts/search?q=${props.category}`, {});
     const [postResults, setPostResults] = useState([]);
     console.log(data);
+
+
 
     return (
         <div>
