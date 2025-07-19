61d8c44a56d8cb202c83cb1405684aa94a3f7f14
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-18 23:38:03 -0400

    wrote onClick handler in postResults

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -6,8 +6,8 @@ function SearchResults(props, { sendSelectedPostToHome }) {
     const { data, error, pending } = useFetch(`https://dummyjson.com/posts/search?q=${props.category}`, {});
     const [postResults, setPostResults] = useState([]);
 
-    function handlePostClick() {
-        sendSelectedPostToHome();
+    function handlePostClick(postId) {
+        sendSelectedPostToHome(postId);
     }
 
     useEffect(() => {
@@ -21,7 +21,7 @@ function SearchResults(props, { sendSelectedPostToHome }) {
             <h2>Search Results for ({props.category})</h2>
             { postResults && postResults.map((item) => {
                 return (
-                    <div key={item.id} className="post-result">
+                    <div key={item.id} className="post-result" onClick={() => handlePostClick(item.id)}>
                         <h3>Post #{item.id}</h3>
                         <p>{item.title}</p>
                     </div>
