43ee3aebacecfce0bbaa7ef80026a6c5b6d5304d
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-19 23:49:39 -0400

    changed api call to get all posts, instead of just first 30

--- src/ApiCalls.jsx
+++ src/ApiCalls.jsx
@@ -1,3 +1,3 @@
-export const getPosts = "https://dummyjson.com/posts";
+export const getPosts = "https://dummyjson.com/posts?limit=0";
 
 export const getPostTags = "https://dummyjson.com/posts/tag-list";
