a11efecc245b15369f3679e69dd230b04f13421c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-21 14:00:08 -0400

    added icons8 attribution to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
     return (
         <div className="footer">
             <p>Data provided by <a href="https://dummyjson.com/">DummyJSON</a></p>
+            <p><a target="_blank" href="https://icons8.com/icon/9i9HlS0bGONj/pencil">Pencil</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
         </div>
     )
 }
