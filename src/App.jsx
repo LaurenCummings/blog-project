0d0fd7700b67844fb55893af7457bddf54a714ec
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-21 13:55:22 -0400

    added Footer to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from '../src/pages/Home';
+import Footer from '../src/components/Footer';
 
 function App() {
 
   return (
     <div>
       <Home />
+      <Footer />
     </div>
   )
 }
